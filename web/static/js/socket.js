2c8ea50eeeff5830ccb8e9fa386ded331c7852ed
MQ <qucool@gmail.com>
2016-04-05 10:52:00 +0800

    Listen channel event

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -59,4 +59,8 @@ channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
+channel.on("test_port", payload => {
+  console.log(payload)
+})
+
 export default socket
