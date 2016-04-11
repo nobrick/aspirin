0b1190ba8ced418940577aadce93642134c54ee6
MQ <qucool@gmail.com>
2016-04-11 17:20:11 +0800

    Add monitor event identity class for index page

--- web/views/monitor_event_view.ex
+++ web/views/monitor_event_view.ex
@@ -1,3 +1,7 @@
 defmodule Aspirin.MonitorEventView do
   use Aspirin.Web, :view
+
+  def monitor_event_identity_class(event) do
+    String.replace("#{event.type}-#{event.addr}-#{event.port}", ".", "_")
+  end
 end
